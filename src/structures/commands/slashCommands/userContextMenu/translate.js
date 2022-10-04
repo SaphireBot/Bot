b078bb739e4d7a392c138c3dc684fee52c4c681f
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2022-10-03 21:57:25 -0300

    little limit fix

--- src/structures/commands/slashCommands/userContextMenu/translate.js
+++ src/structures/commands/slashCommands/userContextMenu/translate.js
@@ -16,14 +16,17 @@ export default {
     async execute({ interaction, e, client }) {
 
         const { targetMessage, locale } = interaction
-        const text = targetMessage.content?.slice(0, 1000)
         const formatedLocale = locale.split('-')[0]
+        let text = targetMessage.content
 
         if (!text)
             return await interaction.reply({
                 content: `${e.Deny} | Não há nenhum texto para traduzir nesta mensagem.`,
                 ephemeral: true
             })
+
+        if (text.length > 1013)
+            text = `${text.slice(0, 1010)}...`
 
         const Embed = {
             color: 0x4295FB,
@@ -39,6 +42,9 @@ export default {
         return translate(text, { to: formatedLocale })
             .then(async res => {
 
+                if (res.text.length > 1013)
+                    res.text = `${res.text.slice(0, 1010)}...`
+
                 Embed.fields[1] = {
                     name: 'Tradução',
                     value: `\`\`\`txt\n${res.text}\n\`\`\``
@@ -46,17 +52,23 @@ export default {
 
                 Embed.footer = { text: `Traduzido de ${Languages[res.from.language.iso]} para ${Languages[formatedLocale]}` }
 
-                return await interaction.editReply({ embeds: [Embed] }).catch(() => { })
+                return await interaction.editReply({ embeds: [Embed] })
 
-            }).catch(async err => {
+            })
+            .catch(async err => {
+
+                let errText = `${err}`
+
+                if (!errText.length > 1013)
+                    errText = `${errText.slice(0, 1010)}...`
 
                 Embed.color = client.red
                 Embed.fields[1] = {
                     name: 'Erro',
-                    value: `\`\`\`txt\n${err}\n\`\`\``
+                    value: `\`\`\`txt\n${errText}\n\`\`\``
                 }
 
-                return await interaction.editReply({ embeds: [Embed] }).catch(() => { })
+                return await interaction.editReply({ embeds: [Embed] })
             })
     }
 }
