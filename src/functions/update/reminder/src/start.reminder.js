9fa8bd1b85b72efc1e1b13f63fc83438147e4114
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2023-01-17 18:14:38 -0300

    fixing and improving reminder starter and notifier

--- src/functions/update/reminder/src/start.reminder.js
+++ src/functions/update/reminder/src/start.reminder.js
@@ -9,52 +9,62 @@ export default async ({ user, data }) => {
     if (typeof user === 'string')
         user = await client.users.fetch(user || '0').catch(() => null)
 
-    const RemindMessage = data.RemindMessage.slice(0, 3500)
+    const RemindMessage = data?.RemindMessage?.slice(0, 3500)
+
+    if (!user || !RemindMessage)
+        return managerReminder.remove(data.id)
+
     const { isAutomatic } = data
     const guild = await client.guilds.fetch(data.guildId).catch(() => null)
-    const Channel = await guild?.channels.fetch(data.ChannelId || "0").catch(() => NotifyUser(user, RemindMessage, data.id))
+    const Channel = await guild?.channels.fetch(data.ChannelId || "0").catch(() => null)
+    const hasMember = await guild?.members.fetch({ user: user.id, cache: true, force: true }).catch(() => null)
 
-    if (!Channel || !Channel.guild || !Channel.guild?.members.cache.has(user.id))
+    if (!guild || !Channel || !hasMember)
         return NotifyUser(user, RemindMessage, data.id)
 
     const msg = await Channel.send(`${e.Notification} | ${user}, lembrete pra você.\n🗒️ | **${RemindMessage}**`)
-        .catch(async () => {
-            NotifyUser()
-            return null
-        })
+        .catch(async () => await  NotifyUser())
 
     if (!msg) return
     if (isAutomatic) return managerReminder.remove(data.id)
 
     managerReminder.setAlert(data.id)
 
-    const emojis = ['📅', '🗑️']
+    const emojis = ['💤', '📅', '🗑️']
 
     for (let i of emojis) msg?.react(i).catch(() => { })
 
     const collector = msg.createReactionCollector({
         filter: (reaction, u) => emojis.includes(reaction.emoji.name) && u.id === user.id,
         idle: 600000,
-        errors: ['idle']
+        max: 1,
+        errors: ['idle', 'max']
     })
-        .on('collect', (reaction) => {
+        .on('collect', async (reaction) => {
 
-            if (reaction.emoji.name === emojis[0]) {
+            const { emoji } = reaction
+
+            if (emoji.name === emojis[1]) {
                 msg.delete().catch(() => { })
                 return revalidateTime(Channel, user, data)
             }
 
-            if (reaction.emoji.name === emojis[1]) {
-                msg.reactions.removeAll().catch(() => { })
+            collector.stop()
+            await msg.reactions.removeAll().catch(() => { })
+
+            if (emoji.name === emojis[2]) {
                 managerReminder.remove(data.id)
-                collector.stop()
                 return msg.edit(`${e.Notification} | ${user}, lembrete pra você.\n🗒️ | **${RemindMessage}**\n${e.Info} | Lembrete deletado.`)
             }
+
+            if (emoji.name === emojis[0])
+                return managerReminder.snooze(msg, data.id, user)
 
             return
         })
         .on('end', (_, reason) => {
-            if (reason !== 'user') return
+            msg.reactions.removeAll().catch(() => { })
+            if (['user', 'limit'].includes(reason)) return
             managerReminder.remove(data.id)
             if (!msg) return
             return msg.edit(`${e.Notification} | ${user}, lembrete pra você.\n🗒️ | **${RemindMessage}**\n${e.Info} | Lembrete deletado.`).catch(() => { })
