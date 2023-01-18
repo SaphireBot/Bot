688dcafd622009fe0c926eaf0c489e6c9a5b45c8
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2023-01-17 21:44:17 -0300

    high upgrade

--- src/functions/plugins/timeMs.js
+++ src/functions/plugins/timeMs.js
@@ -1,11 +1,78 @@
 import moment from "moment-timezone"
 
-export default (Time) => {
+export default Time => {
 
-    const Args = Time?.toLowerCase()?.trim().split(/ +/g)
+    if (Time == "00:00") Time = 'tomorrow 00:00'
+
+    let Args = Time?.toLowerCase()?.trim().split(/ +/g)
     let timeResult = 0
+    const today = new Date().getDay()
 
-    if (Args[0].includes('/') || Args[0].includes(':') || ['hoje', 'today', 'tomorrow', 'amanhã'].includes(Args[0]?.toLowerCase())) {
+    const week = {
+        domingo: 0, sunday: 0,
+        segunda: 1, 'segunda-feira': 1, monday: 1,
+        terça: 2, 'terça-feira': 2, tuesday: 2,
+        quarta: 3, 'quarta-feira': 3, wednesday: 3,
+        quinta: 4, 'quinta-feira': 4, thursday: 4,
+        sexta: 5, 'sexta-feira': 5, friday: 5,
+        sabado: 6, sábado: 6, saturday: 6,
+        hoje: today + 1, today: today + 1, tomorrow: today + 1, amanhã: today + 1
+    }[Args[0]?.toLowerCase()]
+
+    if (week !== undefined)
+        return calculateWeek()
+
+    return Args[0].includes('/') || Args[0].includes(':') ? withDay() : minimalDay()
+
+    function minimalDay() {
+        for (let i = 0; i < Args.length; i++) {
+
+            if (['a', 'y'].includes(Args[i].at(-1)) || ['ano', 'year', 'anos', 'y'].includes(Args[i + 1])) {
+                const string = ['a', 'y'].includes(Args[i].at(-1)) ? `${Args[i]}` : `${Args[i]}${Args[i + 1]}`
+                let time = formatString(string) * 60 * 60 * 24 * 365
+                if (isNaN(time)) return null
+                timeResult += parseInt(time)
+                continue
+            }
+
+            if (Args[i].at(-1).includes('d') || ['dias', 'dia', 'day', 'days', 'd'].includes(Args[i + 1])) {
+                const string = Args[i].at(-1).includes('d') ? `${Args[i]}` : `${Args[i]}${Args[i + 1]}`
+                let time = formatString(string) * 60 * 60 * 24
+                if (isNaN(time)) return null
+                timeResult += parseInt(time)
+                continue
+            }
+
+            if (Args[i].slice(-1).includes('h') || ['horas', 'hora', 'hour', 'hours', 'h'].includes(Args[i + 1])) {
+                const string = Args[i].at(-1).includes('h') ? `${Args[i]}` : `${Args[i]}${Args[i + 1]}`
+                let time = formatString(string) * 60 * 60
+                if (isNaN(time)) return null
+                timeResult += parseInt(time)
+                continue
+            }
+
+            if (Args[i].slice(-1).includes('m') || ['minuto', 'minutos', 'minute', 'minutes', 'm'].includes(Args[i + 1])) {
+                const string = Args[i].at(-1).includes('m') ? `${Args[i]}` : `${Args[i]}${Args[i + 1]}`
+                let time = formatString(string) * 60
+                if (isNaN(time)) return null
+                timeResult += parseInt(time)
+                continue
+            }
+
+            if (Args[i].slice(-1).includes('s') || ['segundo', 'segundos', 'second', 'seconds', 's'].includes(Args[i + 1])) {
+                const string = Args[i].at(-1).includes('s') ? `${Args[i]}` : `${Args[i]}${Args[i + 1]}`
+                let time = formatString(string)
+                if (isNaN(time)) return null
+                timeResult += parseInt(time)
+                continue
+            }
+
+            continue
+        }
+        return timeResult
+    }
+
+    function withDay() {
 
         let data = Args[0]
         let hour = Args[1]
@@ -30,60 +97,28 @@ export default (Time) => {
         const hourArray = hour.split(':')
         const dia = parseInt(dataArray[0])
         const mes = parseInt(dataArray[1]) - 1
-        const ano = parseInt(dataArray[2]) || '2023'
-        const hora = parseInt(hourArray[0])
-        const minutos = parseInt(hourArray[1])
+        const ano = parseInt(dataArray[2]) || new Date().getFullYear()
+        const hora = parseInt(hourArray[0]) || 0
+        const minutos = parseInt(hourArray[1]) || 0
         const segundos = parseInt(hourArray[2]) || 0
 
         let date = moment.tz({ day: dia, month: mes, year: ano, hour: hora, minutes: minutos, seconds: segundos }, "America/Sao_Paulo")
-
         if (!date.isValid()) return null
-
         date = date.valueOf()
-
         if (date < Date.now()) return false
-
         timeResult += date - Date.now()
 
-    } else
-        for (let i = 0; i < Args.length; i++) {
+        return timeResult
+    }
 
-            if (Args[i].at(-1).includes('d') || ['dias', 'dia', 'day', 'days'].includes(Args[i + 1])) {
-                const string = Args[i].at(-1).includes('d') ? `${Args[i]}` : `${Args[i]}${Args[i + 1]}`
-                let time = formatString(string) * 60 * 60 * 24
-                if (isNaN(time)) return null
-                timeResult += parseInt(time)
-                continue
-            }
+    function calculateWeek() {
 
-            if (Args[i].slice(-1).includes('h') || ['horas', 'hora', 'hour', 'hours'].includes(Args[i + 1])) {
-                const string = Args[i].at(-1).includes('h') ? `${Args[i]}` : `${Args[i]}${Args[i + 1]}`
-                let time = formatString(string) * 60 * 60
-                if (isNaN(time)) return null
-                timeResult += parseInt(time)
-                continue
-            }
+        const date = new Date()
+        date.setDate(date.getDate() + (week - 1 - date.getDay() + 7) % 7 + 1);
+        Args = `${date.toLocaleDateString("pt-BR")} ${Args.slice(1).join(' ') || '12:00'}`.split(/ +/g)
+        return withDay()
+    }
 
-            if (Args[i].slice(-1).includes('m') || ['minuto', 'minutos', 'minute', 'minutes'].includes(Args[i + 1])) {
-                const string = Args[i].at(-1).includes('m') ? `${Args[i]}` : `${Args[i]}${Args[i + 1]}`
-                let time = formatString(string) * 60
-                if (isNaN(time)) return null
-                timeResult += parseInt(time)
-                continue
-            }
-
-            if (Args[i].slice(-1).includes('s') || ['segundo', 'segundos', 'second', 'seconds'].includes(Args[i + 1])) {
-                const string = Args[i].at(-1).includes('s') ? `${Args[i]}` : `${Args[i]}${Args[i + 1]}`
-                let time = formatString(string)
-                if (isNaN(time)) return null
-                timeResult += parseInt(time)
-                continue
-            }
-
-            continue
-        }
-
-    return timeResult
 }
 
 function day(tomorrow = false) {
@@ -108,26 +143,36 @@ function formatString(string) {
     return string
         .replace(/(\D+)/, str => {
             if ([
-                'd',
-                'dia',
-                'dias',
-                'day',
-                'days',
-                'h',
-                'hora',
-                'horas',
-                'hour',
-                'hours',
+                's',
+                'segundo',
+                'segundos',
+                'second',
+                'seconds',
+
                 'm',
                 'minuto',
                 'minutos',
                 'minutes',
                 'minute',
-                's',
-                'segundo',
-                'segundos',
-                'second',
-                'seconds'
+
+                'h',
+                'hora',
+                'horas',
+                'hour',
+                'hours',
+
+                'd',
+                'dia',
+                'dias',
+                'day',
+                'days',
+
+                'a',
+                'ano',
+                'anos',
+                'y',
+                'year',
+                'years',
             ].includes(str)) return '000'
         })
 }
