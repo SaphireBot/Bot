b9acd7371b156852095c2f79bf3a6490c27cddec
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2023-02-21 20:01:02 -0300

    backup command

--- src/structures/commands/slashCommands/admin/admin/backup.admin.js
+++ src/structures/commands/slashCommands/admin/admin/backup.admin.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 import { Emojis as e } from "../../../../../util/util.js"
-import { readFileSync, rmSync, createWriteStream } from 'fs'
+import { readFileSync, createWriteStream, rm } from 'fs'
 import { AttachmentBuilder } from "discord.js"
-import { cwd } from "process"
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 export default async interaction => {
 
@@ -14,29 +14,44 @@ export default async interaction => {
 
     await interaction.reply({ content: `${e.Loading} | Obtendo o backup da Saphire API...`, ephemeral: true })
 
-    const backup = await axios({
-        url: "https://ways.discloud.app/backup",
-        headers: { authorization: password },
-        responseType: "stream"
-    })
-        .then(response => {
-            response.data.pipe(createWriteStream('backup.zip'))
-            return true
-        })
-        .catch(error => {
-            console.log(error)
-            return false
-        })
+    const backup = await getBackup()
+    await delay(2000)
 
     if (!backup)
         return await interaction.editReply({ content: `${e.Deny} | Não foi possível obter o backup.` })
 
-    const file = readFileSync(`${cwd()}/backup.zip`);
-    const attachment = new AttachmentBuilder(file, { name: "database-backup.zip", description: "Backup do banco de dados" })
+    let file = undefined
+    let attachment = []
 
-    await user.send({ content: `${e.Check} | Backup solicitado com sucesso.`, files: [attachment] })
-        .then(async () => await interaction.editReply({ content: `${e.Check} | Backup solicitado com sucesso. Enviado ao seu privado.` }))
-        .catch(async error => await interaction.channel.send({ content: `${e.Deny} | Não foi possível mandar o backup para o seu privado.\n${e.bug} | \`${error}\`` }))
+    try {
+        file = readFileSync("backup.zip")
+        attachment.push(new AttachmentBuilder(file, { name: "database-backup.zip", description: "Backup do banco de dados" }))
+    } catch (err) {
+        if (file) rm("backup.zip", () => { })
+        return await interaction.editReply({ content: `${e.DenyX} | Não foi possível obter o arquivo.` })
+    }
 
-    return rmSync("backup.zip")
+    return rm("backup.zip", async err => {
+        return await user.send({ content: `${e.CheckV} | Backup solicitado com sucesso.`, files: attachment })
+            .then(async () => await interaction.editReply({ content: `${e.CheckV} | Backup solicitado com sucesso. Enviado ao seu privado.${err ? `${e.DenyX} | Arquivo não deletado da raiz` : ""}` }))
+            .catch(async error => await interaction.channel.send({ content: `${e.DenyX} | Não foi possível mandar o backup para o seu privado.\n${e.bug} | \`${error}\`` }))
+    })
+
+    async function getBackup() {
+
+        const file = await axios({
+            url: "https://ways.discloud.app/backup",
+            headers: { authorization: password },
+            responseType: "stream"
+        })
+            .then(response => response.data)
+            .catch(() => false)
+
+        if (file) {
+            await file.pipe(createWriteStream('backup.zip'))
+            return true
+        }
+
+        return false
+    }
 }
