edf4e44f5a1a97c146089c3ec70e227b92f1d8dd
Rodrigo <rodrigo.couto.santos@hotmail.com>
2023-03-14 21:54:08 -0300

    otimizing

--- src/structures/events/guildCreate.js
+++ src/structures/events/guildCreate.js
@@ -1,7 +1,4 @@
-import {
-    SaphireClient as client,
-    Database
-} from '../../classes/index.js'
+import { SaphireClient as client, Database } from '../../classes/index.js'
 import { Config as config } from '../../util/Constants.js'
 import { Emojis as e } from '../../util/util.js'
 
@@ -16,20 +13,17 @@ client.on("guildCreate", async guild => {
     const blacklistServers = clientData?.Blacklist?.Guilds || []
 
     if (blacklistServers.some(data => data?.id === guild.id))
-        return guild.leave().catch(async err => {
-            const owner = await client.users.fetch(config.ownerId).catch(() => null)
-            return owner?.send(`${e.Deny} | Não foi possível sair da ${guild.id} \`${guild.id}\` que está na blacklist.\n> \`${err}\``).catch(() => { })
-        })
+        return guild.leave()
+            .catch(async err => {
+                const owner = await client.users.fetch(config.ownerId).catch(() => null)
+                return owner?.send(`${e.Deny} | Não foi possível sair da ${guild.id} \`${guild.id}\` que está na blacklist.\n> \`${err}\``).catch(() => { })
+            })
 
-    Hello();
     const server = await Database.Guild.findOne({ id: guild.id })
     if (!server) await Database.registerServer(guild)
 
-    async function Hello() {
-        const FirstMessageChannel = guild.channels.cache.find(channel => channel.isTextBased() && channel.permissionsFor(guild.members.me).has('SendMessages'))
+    const FirstMessageChannel = guild.channels.cache.find(channel => channel.isTextBased() && channel.permissionsFor(guild.members.me).has('SendMessages'))
+    if (!FirstMessageChannel) return
+    return FirstMessageChannel.send(`${e.NezukoDance} | Oooie, eu sou a ${client.user.username}.\n${e.Stonks} | Meu prefiro padrão é \`/\`, todos os meus são em Slash Commands.`).catch(() => { })
 
-        if (!FirstMessageChannel) return
-
-        return FirstMessageChannel.send(`${e.NezukoDance} | Oooie, eu sou a ${client.user.username}.\n${e.Stonks} | Meu prefiro padrão é \`/\`, todos os meus são em Slash Commands.`).catch(() => { })
-    }
 })
