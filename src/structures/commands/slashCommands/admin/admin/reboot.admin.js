4f45b8223a915cd2a3f68527d8cc753f59126bd0
Rodrigo <rodrigo.couto.santos@hotmail.com>
2023-03-27 21:13:47 -0300

    inline

--- src/structures/commands/slashCommands/admin/admin/reboot.admin.js
+++ src/structures/commands/slashCommands/admin/admin/reboot.admin.js
@@ -1,7 +1,4 @@
-import {
-    Database,
-    SaphireClient as client
-} from "../../../../../classes/index.js"
+import { Database, SaphireClient as client } from "../../../../../classes/index.js"
 import { Config } from "../../../../../util/Constants.js"
 import { Emojis as e } from "../../../../../util/util.js"
 
@@ -18,10 +15,7 @@ export default async interaction => {
     const message = options.getString('message') || 'Nenhum dado informado.'
     client.restart = message
 
-    const msg = await interaction.reply({
-        content: `${e.Loading} | Reboot inicializado.`,
-        fetchReply: true
-    })
+    const msg = await interaction.reply({ content: `${e.Loading} | Reboot inicializado.`, fetchReply: true })
 
     await Database.Cache.Client.set('Restart', {
         channelId: channel.id,
