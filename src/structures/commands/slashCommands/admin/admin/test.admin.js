02284a2d152cb99d3d75d3ec6e9cc3f75617abaf
Rodrigo <rodrigo.couto.santos@hotmail.com>
2023-04-14 22:19:14 -0300

    somes comments

--- src/structures/commands/slashCommands/admin/admin/test.admin.js
+++ src/structures/commands/slashCommands/admin/admin/test.admin.js
@@ -2,19 +2,27 @@ import twitch from '../../../../../functions/update/twitch/manager.twitch.js'
 
 export default async interaction => {
 
-    setInterval(() => request(), 500)
-    setInterval(() => request(), 500)
-    setInterval(() => request(), 500)
-    setInterval(() => request(), 500)
-    setInterval(() => request(), 500)
-    setInterval(() => request(), 500)
+    // setInterval(() => request(), 500)
+    // setInterval(() => request(), 500)
+    // setInterval(() => request(), 500)
+    // setInterval(() => request(), 500)
+    // setInterval(() => request(), 500)
+    // setInterval(() => request(), 500)
+    // setInterval(() => request(), 500)
+    // setInterval(() => request(), 500)
+    // setInterval(() => request(), 500)
+    // setInterval(() => request(), 500)
+    // setInterval(() => request(), 500)
+    // setInterval(() => request(), 500)
+    // setInterval(() => request(), 500)
+    // setInterval(() => request(), 500)
 
-    function request() {
-        twitch.fetcher('https://api.twitch.tv/helix/users?login=alanzoka')
-            .then(data => {
-                if (!Array.isArray(data)) console.log(data)
-                else console.log('Respondido')
-            })
-    }
+    // function request() {
+    //     twitch.fetcher('https://api.twitch.tv/helix/users?loagin=alanzoka')
+    //         .then(data => {
+    //             if (!Array.isArray(data)) console.log(data)
+    //             else console.log('Respondido')
+    //         })
+    // }
 
 }
